a07a3571f9ea98bc31b3cafdf1cd0c85f2b1b7dc
ZettaRy <insptso7ovo@gmail.com>
2019-08-31 23:02:40 +0300

    incapsulation

---
+++ snake/snake/Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point()
+        {
+
+        }
+
+        public Point (int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+
+        }
+    }
+}
